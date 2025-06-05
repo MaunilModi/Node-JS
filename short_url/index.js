6a03b155354c2ee2621b4efaaf4ebf1b4cba9a6f
MaunilModi98 <202201490@daiict.ac.in>
2025-06-05 11:47:42 +0530

    Login & Signup

--- short_url/index.js
+++ short_url/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const path = require("path");
-const urlRouter = require("./routes/url");
 const { connectToMongoDB } = require("./connect");
-const staticRoute = require("./routes/staticRouter");
+
 const PORT = 8001;
 const app = express();
 const URL = require("./models/url");
+
+const urlRouter = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
+const userRoute = require("./routes/user");
 
 connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(() =>
   console.log("Connect MongoDB")
@@ -16,7 +19,9 @@ app.set("views", path.resolve("./views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use("/url", urlRouter);
+app.use("/user", userRoute);
 app.use("/", staticRoute);
 
 app.get("/url/:shorId", async (req, res) => {
